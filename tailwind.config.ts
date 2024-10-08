5b8c54847f2cc2b247e740ad2b4aeda42f3c13eb
Neto Moraes <jomoraesneto@gmail.com>
2024-10-08 11:24:18 -0300

    feat: change config files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,16 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        roboto: ["var(--font-roboto)", "sans-serif"],
+        poppins: ["var(--font-poppins)", "sans-serif"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
