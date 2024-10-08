d8f685e6f75ab51e4a19c141a26d18602f5356f7
Neto Moraes <jomoraesneto@gmail.com>
2024-10-08 11:19:16 -0300

    feat: add api caller file

---
+++ src/apiClient/apiCaller.ts
@@ -0,0 +1,42 @@
+import axios, { AxiosHeaders, AxiosInstance, AxiosResponse } from "axios";
+
+const api: AxiosInstance = axios.create({
+  baseURL: process.env.API_ROUTE,
+  timeout: 15000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (!config.headers) {
+      config.headers = new AxiosHeaders();
+    }
+
+    if (token) {
+      config.headers.set("Authorization", `Bearer ${token}`);
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+api.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      console.error("Unauthorized, logging out...");
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default api;
