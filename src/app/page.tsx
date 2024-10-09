b8c665f350853957e26cf4bd702eac82b2d557a9
Neto Moraes <jomoraesneto@gmail.com>
2024-10-09 00:32:14 -0300

    feat: change token name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,16 @@ const HomePage = () => {
   const router = useRouter();
 
   useEffect(() => {
-    // Pegando o token de autenticação do localStorage
-    const token = localStorage.getItem("auth-token"); // Ajuste 'auth-token' para o nome da chave que você usa para armazenar o token
+    const token = localStorage.getItem("auth-token");
 
-    // Verifica se o usuário está logado com base no token
     if (token) {
-      // Se o token existir, redireciona para a página home
       router.push("/home");
     } else {
-      // Se o token não existir, redireciona para a página de login
       router.push("/login");
     }
   }, [router]);
 
-  return null; // Enquanto faz o redirecionamento, não renderiza nada (pode adicionar um spinner, se quiser)
+  return null;
 };
 
 export default HomePage;
