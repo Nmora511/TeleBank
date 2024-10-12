43d01769a399b3fbb8e0657d3085885744459a9d
Neto Moraes <jomoraesneto@gmail.com>
2024-10-12 17:08:12 -0300

    fix: scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import "./globals.css";
 import { Roboto, Poppins } from "next/font/google";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import ScrollableWrapper from "@/components/ScrollableWrapper";
 
 const roboto = Roboto({
   weight: ["400", "700"],
