5949ab1e3756b09b9a1dcbb8772a508f92ee77ee
Neto Moraes <jomoraesneto@gmail.com>
2024-10-12 16:13:42 -0300

    feat: add css property

--- src/components/UtilComponents/LoadingButton/LoadingButton.tsx
+++ src/components/UtilComponents/LoadingButton/LoadingButton.tsx
@@ -3,10 +3,12 @@ import ThreeDotsWave from "./ThreeDotsWave";
 
 type ButtonProps = {
   isLoadingButton: boolean;
+  css?: string;
 } & React.ButtonHTMLAttributes<HTMLButtonElement> &
   MotionProps;
 
 export default function LoadingButton({
+  css,
   isLoadingButton,
   children,
   ...rest
@@ -17,7 +19,7 @@ export default function LoadingButton({
       disabled={isLoadingButton}
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.9 }}
-      className="bg-[var(--primary-yellow)] mb-4 hover:bg-[var(--secondary-yellow)] active:bg-[var(--tertiary-yellow)] font-bold p-4 rounded-2xl"
+      className={`bg-[var(--primary-yellow)] mb-4 hover:bg-[var(--secondary-yellow)] active:bg-[var(--tertiary-yellow)] font-bold p-4 rounded-2xl ${css}`}
     >
       {isLoadingButton ? <ThreeDotsWave /> : children}
     </motion.button>
