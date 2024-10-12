d2032dc60d471175f7fe4b4cd812c3cf6e1eba00
Neto Moraes <jomoraesneto@gmail.com>
2024-10-12 16:13:19 -0300

    feat: add @

--- src/components/homeComponents/Card.tsx
+++ src/components/homeComponents/Card.tsx
@@ -18,9 +18,9 @@ export default function Card({ friend }: CardProps) {
       ? "text-[var(--foreground)]"
       : balanceColorVerification;
   return (
-    <div className="w-[80%] h-[26%] m-4 bg-[var(--secondary-foreground)] border-[var(--primary-yellow)] border-[0.2rem] rounded-lg">
-      <h1 className="font-bold text-[12vw] truncate mx-5">{friend.name}</h1>
-      <h2 className="mb-2">{friend.username}</h2>
+    <div className="flex flex-col items-center justify-center cursor-pointer w-[80%] h-[26%] m-4 bg-[var(--secondary-foreground)] border-[var(--primary-yellow)] border-[0.2rem] rounded-lg">
+      <h1 className="font-bold text-[10vw] truncate mx-5">{friend.name}</h1>
+      <h2 className="mb-2">@{friend.username}</h2>
       <h2 className="text-[7vw]">Saldo</h2>
       <h2 className={`text-[8vw] font-bold ${balanceColor}`}>
         {moneyMask(friend.balance)}
