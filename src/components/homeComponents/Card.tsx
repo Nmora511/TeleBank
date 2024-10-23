367d9a789c5bcfeb4a36f9e70a4d7f012f52226e
Neto Moraes <jomoraesneto@gmail.com>
2024-10-23 01:39:03 -0300

    feat: add friend card router redirector

--- src/components/homeComponents/Card.tsx
+++ src/components/homeComponents/Card.tsx
@@ -1,4 +1,6 @@
+"use client";
 import { moneyMask } from "@/utils/MoneyMask";
+import { useRouter } from "next/navigation";
 
 type CardProps = {
   friend: {
@@ -9,6 +11,8 @@ type CardProps = {
 };
 
 export default function Card({ friend }: CardProps) {
+  const router = useRouter();
+
   const balanceColorVerification =
     friend.balance > 0
       ? "text-[var(--primary-green)]"
@@ -18,7 +22,12 @@ export default function Card({ friend }: CardProps) {
       ? "text-[var(--foreground)]"
       : balanceColorVerification;
   return (
-    <div className="flex flex-col items-center justify-center cursor-pointer w-[80%] h-[26%] m-4 bg-[var(--secondary-foreground)] border-[var(--primary-yellow)] border-[0.2rem] rounded-lg">
+    <div
+      onClick={() => {
+        router.push(`/friend/${friend.username}`);
+      }}
+      className="flex flex-col items-center justify-center cursor-pointer w-[80%] h-[26%] m-4 bg-[var(--secondary-foreground)] border-[var(--primary-yellow)] border-[0.2rem] rounded-lg"
+    >
       <h1 className="font-bold text-[10vw] truncate mx-5">{friend.name}</h1>
       <h2 className="mb-2">@{friend.username}</h2>
       <h2 className="text-[7vw]">Saldo</h2>
