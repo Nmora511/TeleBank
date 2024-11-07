8b2331cecb6c95ae9939bd5febe8d29c46e1e849
Neto Moraes <jomoraesneto@gmail.com>
2024-11-07 10:33:51 -0300

    fix: remove undefined classname error

--- src/components/UtilComponents/Input.tsx
+++ src/components/UtilComponents/Input.tsx
@@ -6,7 +6,7 @@ export default function Input({ css, ...rest }: InputProps) {
   return (
     <input
       {...rest}
-      className={`m-2 p-2 focus:bg-[var(--secondary-foreground)] outline-none border-[1px] rounded-lg border-[var(--foreground)] bg-[var(--background)] ${css}`}
+      className={`m-2 p-2 focus:bg-[var(--secondary-foreground)] outline-none border-[1px] rounded-lg border-[var(--foreground)] bg-[var(--background)] ${css ? css : ""}`}
     />
   );
 }
